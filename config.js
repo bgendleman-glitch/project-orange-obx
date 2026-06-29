3a68da0be92ce6d03a542a12f307369adc5dda41
bgendleman-glitch <bgendleman@gmail.com>
2026-06-29 12:44:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Paste your Google Apps Script Web App URL below.
 // It should look like: https://script.google.com/macros/s/AKfycb.../exec
 window.PROJECT_ORANGE_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyUETgXEbI5OotMfmk15FIfthxyKybS-_ORhseB3txXbeHnkCi1gOXrWFHbRecGyV5y/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbyUETgXEbI5OotMfmk15FIfthxyKybS-_ORhseB3txXbeHnkCi1gOXrWFHbRecGyV5y/exec"
 };
