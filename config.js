a5f0d0c3117fbc077e15b103d35c03f86051bae2
bgendleman-glitch <bgendleman@gmail.com>
2026-06-29 12:07:57 -0400

    Updated config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Project Orange configuration
 // Paste your deployed Google Apps Script Web App URL here after deploying Code.gs.
 window.PROJECT_ORANGE_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxipHSrKS9pKGnzrNuhWtuH6YsinuKIghBspr2pJ3h_Bw-NnLqiypWIwRr1LlLnvf0J/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbycaBgAm_1u_M4vPnrLiRbMALZwWVWyBWLchPt7KIbdHUpmt5FKu5snmEBzuJJpLWtG/exec",
   ACTIVE_SEASON_ID: "S_2026",
   ADMIN_PIN: "orange"
 };
